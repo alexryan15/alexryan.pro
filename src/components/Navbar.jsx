eaf1e63dab38755587cc9e0d853857f7182ef72c
Alex Ryan <alexryan1717@hotmail.com>
2022-04-27 17:01:39 -0400

    project styling improved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,27 +16,27 @@ const Navbar = () => {
       {/* Menu */}
       <ul className="hidden md:flex">
         <li>
-          <Link to="home" smooth={true} duration={500}>
+          <Link className="caret-transparent" to="home" smooth={true} duration={500}>
             Home
           </Link>
         </li>
         <li>
-          <Link to="about" smooth={true} duration={500}>
+          <Link className="caret-transparent" to="about" smooth={true} duration={500}>
             About
           </Link>
         </li>
         <li>
-          <Link to="skills" smooth={true} duration={500}>
+          <Link className="caret-transparent" to="skills" smooth={true} duration={500}>
             Skills
           </Link>
         </li>
         <li>
-          <Link to="projects" smooth={true} duration={500}>
+          <Link className="caret-transparent" to="projects" smooth={true} duration={500}>
             Projects
           </Link>
         </li>
         <li>
-          <Link to="contact" smooth={true} duration={500}>
+          <Link className="caret-transparent" to="contact" smooth={true} duration={500}>
             Contact
           </Link>
         </li>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const Projects = () => {
   return (
@@ -6,41 +6,41 @@ const Projects = () => {
       {/* Container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full ">
         <div>
-          <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+          <p className="caret-transparent text-4xl font-bold inline border-b-4 border-pink-600">
             Projects
           </p>
         </div>
 
         {/* Projects Container */}
-        <div className="w-full grid grid-cols-1 gap-4 text-center py-8 ">
-          <div className="pt-4 rounded-lg border-2 border-pink-600 shadow-md shadow-[#040c16] hover:scale-105 duration-500">
+        <div className="w-full grid grid-cols-1 gap-4 py-8">
+          <div className="leading-7 caret-transparent rounded-lg border-2 border-pink-600 shadow-md shadow-[#040c16] hover:scale-105 duration-500">
             <img className="w-20 mx-auto" />
-            <p className="my-4">Python</p>
+            <p className="m-4">
+                Portfolio Website <br /> 
+                A responsive website that was built using React and styled with Tailwind CSS. <br />
+                <a className="text-pink-600" href="https://github.com/alexryan-15/alexryan-15.github.io">SOURCE CODE</a>
+            </p>
           </div>
-          <div className="pt-4 rounded-lg border-2 border-pink-600 shadow-md shadow-[#040c16] hover:scale-105 duration-500">
+          <div className="leading-7 caret-transparent rounded-lg border-2 border-pink-600 shadow-md shadow-[#040c16] hover:scale-105 duration-500">
             <img className="w-20 mx-auto" />
-            <p className="my-4">Java</p>
+            <p className="m-4">WEB SCRAPER</p>
           </div>
-          <div className="pt-4 rounded-lg border-2 border-pink-600 shadow-md shadow-[#040c16] hover:scale-105 duration-500">
+          <div className="leading-7 caret-transparent rounded-lg border-2 border-pink-600 shadow-md shadow-[#040c16] hover:scale-105 duration-500">
             <img className="w-20 mx-auto" />
-            <p className="my-4">MySQL</p>
+            <p className="m-4">PYTHON LEETCODE SOLUTIONS</p>
           </div>
-          <div className="pt-4 rounded-lg border-2 border-pink-600 shadow-md shadow-[#040c16] hover:scale-105 duration-500">
+          <div className="leading-7 caret-transparent rounded-lg border-2 border-pink-600 shadow-md shadow-[#040c16] hover:scale-105 duration-500">
             <img className="w-20 mx-auto" />
-            <p className="my-4">Github</p>
+            <p className="m-4">ROOM SCHEDULER</p>
           </div>
-          <div className="pt-4 rounded-lg border-2 border-pink-600 shadow-md shadow-[#040c16] hover:scale-105 duration-500">
+          <div className="leading-7 caret-transparent rounded-lg border-2 border-pink-600 shadow-md shadow-[#040c16] hover:scale-105 duration-500">
             <img className="w-20 mx-auto" />
-            <p className="my-4">React</p>
-          </div>
-          <div className="pt-4 rounded-lg border-2 border-pink-600 shadow-md shadow-[#040c16] hover:scale-105 duration-500">
-            <img className="w-20 mx-auto" />
-            <p className="my-4">Tailwind</p>
+            <p className="m-4">MALLOC</p>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
