4d2cbf6c88b40adc0ca76928fdc5a5693c501d36
Alex Ryan <alexryan1717@hotmail.com>
2022-05-09 17:39:16 -0400

    removed view projects button on home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,9 +8,7 @@ const Home = () => {
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full caret-transparent">
         <p className="text-white text-xl">Hi, I'm</p>
-        <h1 className="text-4xl sm:text-6xl font-bold text-white">
-          Alex Ryan
-        </h1>
+        <h1 className="text-4xl sm:text-6xl font-bold text-white">Alex Ryan</h1>
         <h2 className="text-5xl font-bold text-[#ed9637]">
           Computer Science Student
         </h2>
@@ -19,16 +17,6 @@ const Home = () => {
           of 2022, I will be working as a Software Engineer Intern for Paylocity
           in Chicago, IL.
         </p>
-        <div>
-          <Link to="projects" smooth={true} duration={500}>
-          <button className="group text-white rounded-lg border-2 px-6 py-3 my-2 flex items-center hover:bg-[#ed9637] hover:border-[#ed9637]">
-            View Projects
-            <span className="group-hover:rotate-90 duration-500">
-              <HiArrowNarrowRight className="ml-3 scale-105" />
-            </span>
-          </button>
-          </Link>
-        </div>
       </div>
     </div>
   );
