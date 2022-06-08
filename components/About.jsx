ba153ef59dcee11fb9bbaa4882320dc62aedaa00
Alex Ryan <alexryan1717@hotmail.com>
2022-06-08 11:43:57 -0500

    updated styling in about

--- components/About.jsx
+++ components/About.jsx
@@ -9,28 +9,28 @@ const About = () => {
           <h1 className="text-center text-3xl py-4 tracking-widest text-[#7510F7]">
             ABOUT
           </h1>
-          <p className="py-4 text-gray-600 text-xl text-center font-light">
+          <p className="py-8 pt-4 text-gray-600 text-xl text-center font-light">
             Hi, I'm Alex. I'm a junior at Penn State University studying
             Computer Science. I am currently working for Paylocity as a Software
             Engineer Intern on their Performance Conversation team.
           </p>
-          <div className="max-w-[60rem] py-4 md:grid grid-cols-3 text-center border-2">
+          <div className="max-w-[60rem] py-4 md:grid grid-cols-3 text-center rounded-xl border-2 border-[#7510F7]">
             <div>
-              <p>Languages</p>
+              <p className="text-[#7510F7] text-bold py-2">Languages</p>
               <p>Python</p>
               <p>JavaScript</p>
               <p>Java</p>
               <p>C#</p>
               <p>C</p>
             </div>
-            <div>
-              <p>Technologies</p>
+            <div className="border-l border-r border-[#7510F7]">
+              <p className="text-[#7510F7] text-bold py-2">Technologies</p>
               <p>React</p>
               <p>.NET</p>
               <p>SQL</p>
             </div>
             <div>
-              <p>Dev Tools</p>
+              <p className="text-[#7510F7] text-bold py-2">Dev Tools</p>
               <p>Github</p>
               <p>Bitbucket</p>
               <p>Linux</p>
