2d7c00b906519596c848294318287c6bc48fb030
Alex Ryan <alexryan1717@hotmail.com>
2022-07-07 16:08:30 -0500

    updates

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -72,15 +72,6 @@ const Navbar = () => {
           <div>
             <div className="flex w-full items-center justify-between">
               <div></div>
-              {/* TODO: replace image for mobile menu */}
-              {/* <Link href="/">
-                <Image
-                  src="/../public/assets/tailwind.png"
-                  width="87"
-                  height="35"
-                  alt="/"
-                />
-              </Link> */}
               <div
                 onClick={handleNav}
                 className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
