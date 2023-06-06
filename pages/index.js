e8a7db86cfb7bf480a800a1b96f8360463f925d8
Alex Ryan <alexryan1717@hotmail.com>
2023-06-06 11:15:32 -0500

    refactored about section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Navbar from '../components/Navbar/Navbar'
-import About from '../components/About'
+import About from '../components/About/About'
 import Contact from '../components/Contact'
 import Main from '../components/Main'
 import Projects from '../components/Projects'
