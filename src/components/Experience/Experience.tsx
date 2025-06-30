781e1293c1f08e983348b47d4e44f035775e2154
Alex Ryan <alexryan1717@hotmail.com>
2025-06-30 11:27:54 -0500

    V2 done

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,34 +1,36 @@
 import React from "react";
-import ExperienceItem from "./ExperienceItem";
 import "./Experience.css";
+import CSharp from "../../images/csharp.svg";
+import TypeScript from "../../images/typescript.svg";
+import JavaScript from "../../images/javascript.png";
+import Python from "../../images/python.svg";
+import ReactLogo from "../../images/react.png";
+import TeamCity from "../../images/teamcity.png";
+import Datadog from "../../images/datadog.png";
 
-const Experiences = () => {
+const Experience = () => {
   return (
     <div id="experience">
       <div>
         <h1>Experience</h1>
-        <ExperienceItem
-          date="August 2024 - Present"
-          title="Software Engineer"
-          company="Paylocity"
-          description="Continuing development for Learning Management in both web application and mobile systems."
-        />
-        <ExperienceItem
-          date="July 2023 - August 2024"
-          title="Associate Software Engineer"
-          company="Paylocity"
-          description="Assisted in the development and maintenance of Learning Management Systems valued at over $140
-          million, utilizing React/JavaScript for the front-end and C#/.NET for the back-end. Specialized in API development around SCORM-based content and streamlined user experience."
-        />
-        <ExperienceItem
-          date="May 2022 - August 2022"
-          title="Software Engineering Intern"
-          company="Paylocity"
-          description="Worked with the Performance Conversation team in the Performance Management application to improve the experience around employee journals and peer feedback."
-        />
+        <p className="experience-description">
+          I have been working as a full-stack software engineer at <span className="company">Paylocity</span> for over 2 years, 
+          contributing to both frontend and backend development. While I enjoy all aspects of being a developer,
+          my passion lies in the background, particularly in API design, system scalability, and performance. 
+          I enjoy building robust, maintainable systems that can handle real-world load. Here are some of the tools that I frequently use:
+        </p>
+        <div className="images">
+          <img src={CSharp} width="50" height="50" />
+          <img src={TypeScript} width="50" height="50" />
+          <img src={JavaScript} width="50" height="50" />
+          <img src={ReactLogo} width="56" height="50" />
+          <img src={Python} width="50" height="50" />
+          <img src={TeamCity} width="50" height="50" />
+          <img src={Datadog} width="50" height="50" />
+        </div>
       </div>
     </div>
   );
 };
 
-export default Experiences;
+export default Experience;
